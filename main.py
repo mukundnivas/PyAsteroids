6ca277d0e7ccdaa3b82aa89dabcf477602fa29dd
mukundnivas <mukundnivas@gmail.com>
2024-11-09 17:22:58 +1100

    Initialised game loop

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+from constants import *
+
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    game_state = True
+
+    while game_state:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
